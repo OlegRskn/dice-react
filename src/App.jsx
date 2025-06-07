4589d8992bc8c9a81bf5ee69eaa501415d563ea3
OlegRskn <olegrskn@gmail.com>
2025-06-07 19:41:19 +0200

    feature: add logic for second dice

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import Dice, { DICE_COUNT } from "./components/Dice/Dice";
 import Button from "./components/Button/Button";
 
 function getRandomDiceIndex() {
-  return Math.floor(Math.random() * DICE_COUNT);
+  let indexArray = [];
+  const a = (count) => Math.floor(Math.random() * count);
+
+  for (let i = 0; i <= DICE_COUNT; i++) {
+    indexArray.push(a(DICE_COUNT));
+  }
+  return indexArray;
 }
 
 export default function App() {
@@ -11,7 +17,9 @@ export default function App() {
 
   return (
     <>
-      <Dice className="dice__image" diceIndex={diceIndex} />
+      <Dice className="dice__image" diceIndex={diceIndex[0]} />
+      <Dice className="dice__image" diceIndex={diceIndex[1]} />
+
       <Button
         className="button"
         onRoll={() => setDiceIndex(getRandomDiceIndex())}
